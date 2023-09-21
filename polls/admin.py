69a6d223ca0cc447bf4859425da13a8aee70f826
Tareq Albeesh <tareqsyr44@gmail.com>
2023-09-21 10:42:25 +0200

    modifying the admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
+
 from . import models
 # Register your models here.
-admin.site.register([models.Question, models.Choice])
+
+
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date data', {'fields': ['pub_date'], 'classes':'collapse'}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+    search_fields = ['question_text']
+
+
+admin.site.register(models.Question, QuestionAdmin)
+admin.site.register(models.Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 # Create your models here.
 
 
@@ -8,6 +9,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
